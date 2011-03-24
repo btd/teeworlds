7c1d82813764972abe43102f361d393eae2fae0f
btd <bardadymchik@gmail.com>
2011-03-24 13:14:06 +0300

    Now server are builded with CMake

--- src/game/version.h
+++ src/game/version.h
@@ -2,7 +2,7 @@
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
-#include "generated/nethash.c"
+//#include "generated/nethash.c"
 #define GAME_VERSION "0.5 trunk"
-#define GAME_NETVERSION "0.5 " GAME_NETVERSION_HASH
+#define GAME_NETVERSION "0.5 b67d1f1a1eea234e"
 #endif
